74ef7fe94e1606c935e4794e78f1351e3cd7b7b2
Ajay Patel <patel.ajay745@gmail.com>
2025-03-27 20:50:19 -0400

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,20 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import { RuruProvider } from "ruru-ui/provider";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <RuruProvider>{children}</RuruProvider>
+        </RootProvider>
       </body>
     </html>
   );
---
+++ components/myButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+function myButton() {
+  return (
+    <Button className="bg-red-300 rounded-xl" variant="outline">
+      Click me
+    </Button>
+  );
+}
+
+export default myButton;
